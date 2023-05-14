ac16661711ab0122478cccb597ccfbab0dbcf314
Ahmetkcak <ahmet.aliberk07@gmail.com>
2023-05-14 16:47:10 +0300

    add user registration and search bar

--- base/models.py
+++ base/models.py
@@ -1,16 +1,12 @@
-from asyncio import Task
 
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import pre_delete
-from django.dispatch import receiver
 from django.utils import timezone
 
 
 class TaskList(models.Model):
     id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
-    createdDate = models.DateTimeField
+    createdDate = models.DateTimeField(auto_now_add=True)
     updatedDate = models.DateTimeField
     deletedDate = models.DateTimeField
     completionPercentage = models.IntegerField
@@ -19,15 +15,11 @@ class TaskList(models.Model):
         self.updatedDate = timezone.now()
         super(TaskList, self).save(*args, **kwargs)
 
-    @receiver(pre_delete, sender=Task)
-    def update_deleted_date(sender, instance, **kwargs):
-        instance.deletedDate = timezone.now()
-        instance.save()
-
 
 
 class Task(models.Model):
     id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     taskListId = models.ForeignKey(TaskList, on_delete=models.CASCADE, null=True, blank=True)
     createdDate = models.DateTimeField(auto_now_add=True)
     updatedDate = models.DateTimeField
@@ -38,11 +30,6 @@ class Task(models.Model):
     def save(self, *args, **kwargs):
         self.updatedDate = timezone.now()
         super(Task, self).save(*args, **kwargs)
-
-    @receiver(pre_delete, sender=Task)
-    def update_deleted_date(sender, instance, **kwargs):
-        instance.deletedDate = timezone.now()
-        instance.save()
 
     def __str__(self):
         return self.description
