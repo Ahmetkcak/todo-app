2ec63e24130b498985499d79bd42fd8dacc7ad17
Ahmetkcak <ahmet.aliberk07@gmail.com>
2023-05-15 11:25:26 +0300

    update html files and views.py

--- base/views.py
+++ base/views.py
@@ -51,8 +51,10 @@ class TaskList(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
-        context['count'] = context['tasks'].filter(completed=False).count()
-
+        context['complete_task_count'] = context['tasks'].filter(completed=True).count()
+        context['total_task_count'] = context['tasks'].count()
+        if context['total_task_count']:
+            context['completion_percentage'] = (context['tasks'].filter(completed=True).count()/context['tasks'].count()) * 100
         search_input = self.request.GET.get('search-area') or ''
         if search_input:
             context['tasks'] = context['tasks'].filter(description__icontains=search_input)
